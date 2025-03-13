ca05d6e022e0a0b029d965d8eb8424164c14dddf
eziceserver <easydryice@gmail.com>
2025-03-13 08:10:45 +0530

    fix the backend

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-app.post("/api/share", (req, res) => {
-  console.log("Received data:", req.body); // ✅ Log the entire request body
+// connectiong db
+mongoose.connect("mongodb://127.0.0.1:27017/textclones")
+.then(() => console.log("connected"))
+.catch((error) => console.log("error:", error))
+// connectiong db
+
+
+
+
+// making schemaa
+
+const TextSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
+
+
+// making schemaa
+
+// making model
+
+const Texts = mongoose.model("Texts", TextSchema)
+
+// making model
+
+
+
+
+
+
+app.post("/api/share", async (req, res) => {
+  // console.log("Received data1st:", req.body); // ✅ Log the entire request body
 
   // Check if `text` and `code` exist
-  if (!req.body.text || !req.body.code) {
-    return res.status(400).json({ error: "Text and Code are required" });
-  }
+  // if (!req.body.text || !req.body.code) {
+  //   return res.status(400).json({ error: "Text and Code are required" });
+  // }
 
   // Access `text` and `code` safely
   const { text, code } = req.body;
   console.log(`Text: ${text}, Code: ${code}`);
 
   res.status(200).json({ message: "Data received successfully" });
+
+  await Texts.create({
+    text : req.body.text,
+    code : req.body.code
+  })
+
+  console.log ("Data created successfully" );
+
 });
 
 
