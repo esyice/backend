d6f4738ef5e724562927c45d613e20fce1f5e09b
esyice <easydryice@gmail.com>
2025-03-23 03:36:51 +0530

    backend fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
-const port = 3000;
+const port = process.env.PORT  ;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
