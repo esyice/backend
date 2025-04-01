94f9b44c6615302e539896a5c77457cd1c0e7619
eZice <144651926+esyice@users.noreply.github.com>
2025-04-01 22:18:18 +0530

    Update index.js
     url from .env

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const mongoose = require("mongoose");
 const app = express();
 const cron = require("node-cron");
 const port = process.env.PORT;
+const url = process.env.URL:
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // connectiong db
 mongoose
-  .connect("mongodb://127.0.0.1:27017/textclones")
+  .connect(${url})
   .then(() => console.log("connected"))
   .catch((error) => console.log("error:", error));
 // connectiong db
